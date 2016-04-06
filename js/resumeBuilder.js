828b9b4c49945ca176a8023a4a247924c93638cb
vzades <vinay.komal100@gmail.com>
2016-04-06 10:10:22 +0530

    ADDING AFTER DAY 1

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,22 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+ 
+   var tought  = "I am aweosme ";
+  
+  console.log(tought);
+  
+  var  newtought  = tought.replace("aweosme","fun");
+  console.log(newtought);
+  
+  
+  var  fromatedname = HTMLheaderName.replace("%data%","Vinay");
+    var formatedRole = HTMLheaderRole.replace("%data%","software developer")
+  
+  
+   $("#header").prepend(formatedRole);
+  $("#header").prepend(fromatedname);
+  
+
+  
+ 
